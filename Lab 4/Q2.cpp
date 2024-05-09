c3e6159fdfb14e1642aba5265ce1fcec8b214d8a
Tooba Farooq <161237786+Tooba-Farooq@users.noreply.github.com>
2024-05-08 19:58:00 -0700

    Update Q2.cpp

--- Lab 4/Q2.cpp
+++ Lab 4/Q2.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
-#include <cstring>
-#include <cctype>
 
 using namespace std;
 
 void revrseArray(int* arr, int size) {
-    int* p = arr;
-    for (int i = size-1 ; i >= 0; i--) {
-        cout << *(p + i) << " ";
+    int* start = arr;
+    int* end = arr + (size - 1);
+    int temp;
+
+    for (int i = 0; i < size/2; i++) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
     }
+    
 }
 
 
 int main() {
     cout << "Tooba Farooq SE-009" << endl;
-    int arr[] = { 1,2,3,4 };
+    int arr[] = { 1,2,3,4,5,6,7 };
     int size = sizeof(arr) / sizeof(arr[0]);
+
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";   
     }
     cout << endl << "After reversing: ";
+
     revrseArray(arr, size);
-    
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
